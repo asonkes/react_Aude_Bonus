cfe24fe142fbd0e5e1ec9457bc300dfc38195b44
asonkes <audrey.sonkes@gmail.com>
2026-04-02 22:54:43 +0200

    commit

--- src/features/auth/components/RegisterForm.jsx
+++ src/features/auth/components/RegisterForm.jsx
@@ -1,56 +1,71 @@
-import {useId} from "react";
+import { useId } from "react";
 import authService from "../../../services/auth.service";
 import { useNavigate } from "react-router";
 
 export const RegisterForm = () => {
-    
-    const id = useId();
-    const navigate = useNavigate();
+  const id = useId();
+  const navigate = useNavigate();
 
-    // On peut ici utiliser directement une fonction asynchrone
-    // donc on peut récupérer les données sans useEffect
-    const handleRegisterSubmit = async(formData) => {
-      console.log('FormData:', formData);
+  // On peut ici utiliser directement une fonction asynchrone
+  // donc on peut récupérer les données sans useEffect
+  const handleRegisterSubmit = async (formData) => {
+    console.log("FormData:", formData);
 
-      // Conversion des données vers un objet JS
-      // Attention rien n'est vérifié
-      const data = Object.fromEntries(formData.entries());
-      console.log("Data", data);
+    // Conversion des données vers un objet JS
+    // Attention rien n'est vérifié
+    const data = Object.fromEntries(formData.entries());
+    console.log("Data", data);
 
-      //Utiliser le service qui permet de contacter la webAPI
-      await authService.register(data);
+    //Utiliser le service qui permet de contacter la webAPI
+    await authService.register(data);
 
-      navigate('/');
-    }
+    navigate("/");
+  };
 
   return (
-   // action permet de reste le formulaire
-   // action permet de pas refresh
+    // action permet de reste le formulaire
+    // action permet de pas refresh
     <form action={handleRegisterSubmit}>
+      <div>
+        <label htmlFor={id + "email"}>E-mail de l'utilisateur: </label>
+        <input id={id + "email"} name="email" type="email" className="border" />
+      </div>
 
-        <div>
-            <label htmlFor={id + 'email'}>E-mail de l'utilisateur: </label>
-            <input id={id + 'email'} name="email" type="email" className="border"/>
-         </div>
+      <div>
+        <label htmlFor={id + "firstname"}>Prénom: </label>
+        <input
+          id={id + "firstname"}
+          name="firstname"
+          type="text"
+          className="border"
+        />
+      </div>
 
-        <div>
-            <label htmlFor={id + 'firstname'}>Prénom: </label>
-            <input id={id + 'firstname'} name="firstname" type="text" className="border"/>
-         </div>
+      <div>
+        <label htmlFor={id + "lastname"}>Nom: </label>
+        <input
+          id={id + "lastname"}
+          name="lastname"
+          type="text"
+          className="border"
+        />
+      </div>
 
-        <div>
-            <label htmlFor={id + 'lastname'}>Nom: </label>
-            <input id={id + 'lastname'} name="lastname" type="text" />
-         </div>
+      <div>
+        <label htmlFor={id + "password"}>Mot de passe: </label>
+        <input
+          id={id + "password"}
+          name="password"
+          type="password"
+          className="border"
+        />
+      </div>
 
-        <div>
-            <label htmlFor={id + 'password'}>Mot de passe: </label>
-            <input id={id + 'password'} name="password" type="password" className="border"/>
-         </div>
-
-         <div>
-            <button type="submit" className="btn">M'enregistrer</button>
-         </div>
+      <div>
+        <button type="submit" className="btn">
+          M'enregistrer
+        </button>
+      </div>
     </form>
-  )
-}
+  );
+};
